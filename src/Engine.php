d066719dd08ed6e692e74d6c306d34fd07facc3b
root-reboot <VTVTima@mail.ru>
2022-01-15 18:38:20 +0300

    fix

--- src/Engine.php
+++ src/Engine.php
@@ -10,7 +10,7 @@ use function Brain\Games\Games\Gcd\brainGcd;
 
 function gameRun($gameTask, $gameData)
 {
-    line('Welcome to the Brain Game!');
+    line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
     line($gameTask);
